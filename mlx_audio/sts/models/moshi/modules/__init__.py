3657db4bc3c74068a7c9d2e67855b7c3f50a387c
mm65x <mm65x@users.noreply.github.com>
2026-03-16 10:39:13 +0000

    chore(moshi): fully prune unused codec modules (conv, quantization) per review

--- mlx_audio/sts/models/moshi/modules/__init__.py
+++ mlx_audio/sts/models/moshi/modules/__init__.py
@@ -1,20 +1,3 @@
-# Copyright (c) Kyutai, all rights reserved.
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-# flake8: noqa
-"""Modules used for building the models."""
-
-from .conv import (
-    Conv1d,
-    ConvDownsample1d,
-    ConvTranspose1d,
-    ConvTrUpsample1d,
-    NormConv1d,
-    NormConvTranspose1d,
-    StreamableConv1d,
-    StreamableConvTranspose1d,
-)
-from .kv_cache import KVCache, RotatingKVCache
-from .quantization import EuclideanCodebook, SplitResidualVectorQuantizer
-
 from .transformer import Transformer, TransformerConfig
+from .kv_cache import KVCache, RotatingKVCache
+from .conditioner import ConditionFuser, ConditionProvider, ConditionTensor
