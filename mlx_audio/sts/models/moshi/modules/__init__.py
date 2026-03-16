31eb40376d5aab21c6dff89e8969a1e0b19cd7ec
mm65x <mm65x@users.noreply.github.com>
2026-03-16 14:39:08 +0000

    fix(moshi): remove nonexistent ConditionFuser import from modules __init__

--- mlx_audio/sts/models/moshi/modules/__init__.py
+++ mlx_audio/sts/models/moshi/modules/__init__.py
@@ -1,3 +1,3 @@
-from .conditioner import ConditionFuser, ConditionProvider, ConditionTensor
+from .conditioner import ConditionProvider, ConditionTensor
 from .kv_cache import KVCache, RotatingKVCache
 from .transformer import Transformer, TransformerConfig
