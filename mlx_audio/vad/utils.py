b1c900d13920a7b9819c1d3cc4fc0539e2902b77
tian-sweetaylor <153503632+tian-sweetaylor@users.noreply.github.com>
2026-05-18 18:31:57 +0800

    feat(vad): add FSMN model to VAD MODEL_REMAPPING

--- mlx_audio/vad/utils.py
+++ mlx_audio/vad/utils.py
@@ -8,6 +8,9 @@ from mlx_audio.utils import base_load_model
 MODEL_REMAPPING = {
     "silero": "silero_vad",
     "silero-vad": "silero_vad",
+    "fsmn": "fsmn",
+    "fsmn-vad": "fsmn",
+    "fsmn_vad": "fsmn",
 }
 
 
